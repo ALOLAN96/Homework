4f1b55f3fa306f4311259fe0e382c3c3c1a7f679
ALOLAN96 <102942850+ALOLAN96@users.noreply.github.com>
2022-07-30 17:02:56 +0800

    Add files via upload

---
+++ DAY3/5.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	int num, single_digits, ten_digit, hundreds_digit;
+
+
+	printf("Enter a number: ");
+	scanf("%d",&num);
+
+	single_digits = num % 10;
+	ten_digit = (num / 10) % 10;cd CScd Dvi 5.c
+		:wq
+		cdcd 'Bcd 
+		cd Lvi gcd.c
+		IInc
+int num1, num2;, temp
+		
+		printf("Enter two integers: ");
+	
+	scanf("%d % &num1, &num2d;
+			
+			",)main
+		while (n != 0)
+		{
+			temp = n;
+			n = m % n;
+			m = temp;
+		}
+
+	printf("Greastest common divisor:%d\n", m);
+
+
+	hundreds_digit = num / 100;
+	
+	
+	if ((num < 1000) && (num > 99))
+	{
+		if ((single_digits * single_digits * single_digits + ten_digit * ten_digit * ten_digit + hundreds_digit * hundreds_digit * hundreds_digit) == num)
+		{
+			printf("输入的数为水仙花数\n");
+		}
+		else 
+		{
+			printf("输入的数不是水仙花数\n");
+		}
+
+	}
+	else if ((num >= 10) && (num < 100))
+	{
+		if (num % 10 == 0)
+		{
+			printf("被除数为0,无法计算\n");
+		}
+		else
+		{
+		if (ten_digit % single_digits == 0)
+		{
+			printf("能被个位数整除\n");
+		}
+		else
+		{
+			printf("不能被个位数整除\n");
+		}
+		}
+	}
+	else if ((num >= 0) && (num <= 9))
+	{
+		if (num % 2 == 0)
+		{
+			printf("输入的为偶数\n");
+		}
+		else
+		{
+			printf("输入的为奇数\n");
+		}
+
+
+	}
+
+
+
+
+	int num;
+
+	return  number: ;
+	scanf("%d", &num);
+
+	for (int i = 1;<= %um; i ++{
+			{
+			if (year % 400 ==0)
+			{
+			printf("\n");
+			}
+
+
+			else 
+			{
+			if (year % 4 == 0)
+			{
+			printf("\n");
+			}
+			}}
+			else 
+			{
+			print\n");
+			}
+		if (i % 2 ==		a00
+			iate = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + day;
+		}
+
+		{else if (month ==10)
+		{
+			date = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + day;
+		}
+		else if (month ==11)
+		{
+			d
+			if (year % 100 == 0)
+	
+			{
+			if (year % 400 == 0)
+			{
+			printf("runnian ");
+			}
+			}ate = 31 + 29 + 31
+			else if (year % 4 == 0)
+			{
+				printf("runnian" );
+			}{
+				else
+				{
+					printf("pingnina ");
+				}
+			}		printf("runnian");
+			\n + 30 + 31 + 30 + 3
+			else
+			{
+	
+				\n			{printf("pingnina");
+			\n+ 30 + 31 + 30 +day;
---
+++ DAY3/6.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+	
+	int year, month, day, date, logo;
+
+	printf("请输入年月日: ");
+	scanf("%d %d %d", &year, &month, &day);
+
+
+
+	if (year % 100 == 0)
+	{
+		if (year % 400 ==0)
+		{
+			logo = 1;
+		}
+	}
+	else if (year % 4 == 0)
+	{
+		logo = 1;
+	}
+	else
+	{
+		logo = 0;
+	}
+
+
+
+
+	if (logo == 1)
+	{
+		if (month == 1)
+		{
+			date = day;
+		}
+		else if (month == 2)
+		{
+			date = 31 + day;
+		}
+		else if (month == 3)
+		{
+			date = 31 + 29 + day;
+		}
+		else if (month == 4)
+		{
+			date = 31 + 29 + 31 + day;
+		}
+		else if (month == 5)
+		{
+			date = 31 + 29 + 31 + 30 + day;
+		}
+		else if (month == 6)
+		{
+			date = 31 + 29 + 31 + 30 + 31 + day;		
+		}
+		else if (month == 7)
+		{
+			date = 31 + 29 + 31 + 30 + 31 + 30 + day;
+		}
+		else if (month == 8)
+		{
+			date = 31 + 29 + 31 + 30 + 31 + 30 + 31 + day;
+		}
+		else if (month == 9)
+		{
+			date = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + day;
+		}
+		else if (month ==10)
+		{
+			date = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + day;
+		}
+		else if (month ==11)
+		{
+			date = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + day;
+		}
+		else if (month ==12)
+		{
+			date = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 +day;
+		}
+	}
+	else 
+	{
+		if (month == 1)
+		{
+			date = day;
+		}
+		else if (month == 2)
+		{
+			date = 31 + day;
+		}
+		else if (month == 3)
+		{
+			date = 31 + 28 + day;
+		}
+		else if (month == 4)
+		{
+			date = 31 + 28 + 31 + day;
+		}
+		else if (month == 5)
+		{
+			date = 31 + 28 + 31 + 30 + day;
+		}
+		else if (month == 6)
+		{
+			date = 31 + 28 + 31 + 30 + 31 + day;		
+		}
+		else if (month == 7)
+		{
+			date = 31 + 28 + 31 + 30 + 31 + 30 + day;
+		}
+		else if (month == 8)
+		{
+			date = 31 + 28 + 31 + 30 + 31 + 30 + 31 + day;
+		}
+		else if (month == 9)
+		{
+			date = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + day;
+		}
+		else if (month ==10)
+		{
+			date = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + day;
+		}
+		else if (month ==11)
+		{
+			date = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + day;
+		}
+		else if (month ==12)
+		{
+			date = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 +day;
+		}
+	}
+	printf("这是第%d天\n",date);
+	return 0;
+}
